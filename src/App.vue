2ec1deb68e059f3114dcbccfe1baafeb664ce67a
刘德伟 <3151081884@qq.com>
2018-11-19 13:34:28 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,34 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+<template lang="pug">
+  .container
+    transition(name="fade" mode="out-in")
+      router-view
 </template>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+<script type="es6">
+export default {
+  name: 'app',
+};
+</script>
+
+<style lang="stylus">
+.container {
+  min-width: 100vw;
+  min-height 100vh
+  background url("https://imgland.oss-cn-hangzhou.aliyuncs.com/photo/2018/294d2924-6a20-4f75-aa29-cc48ae63305f.png")
+  background-size cover
+  background-position 50%
+  background-repeat no-repeat
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  div, span {
+    font-size: 14px;
+    font-family: SFUIDisplay-Semibold;
+    font-weight: 600;
+    color: rgba(255, 255, 255, 1);
+    line-height: 16px;
+  }
+}
 </style>
---
+++ src/views/index.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  .view
+    .title
+      img(src="https://imgland.oss-cn-hangzhou.aliyuncs.com/photo/2018/47957fec-8abb-41d1-8146-f2cb9ec00971.png")
+    .subTitle Long press the picture below and then share it to your friend
+</template>
+
+<script type="es6">
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="stylus">
+.container {
+  min-width: 100vw;
+  min-height: 100vh;
+  background: url("https://imgland.oss-cn-hangzhou.aliyuncs.com/photo/2018/294d2924-6a20-4f75-aa29-cc48ae63305f.png");
+  background-size: cover;
+  background-position: 50%;
+  background-repeat: no-repeat;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  div, span {
+    font-size: 14px;
+    font-family: SFUIDisplay-Semibold;
+    font-weight: 600;
+    color: rgba(255, 255, 255, 1);
+    line-height: 16px;
+  }
+}
+</style>
